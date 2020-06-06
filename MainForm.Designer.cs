b5298ef3495f28b117728e546aa7b5d1c3b3e8c4
ngan <thanhnganvo240@gmail.com>
2020-06-06 09:23:51 +0700

    Upload repo

---
+++ MainForm.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace KidleTeam8
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ColumnHeader cName;
+            System.Windows.Forms.ColumnHeader cPath;
+            System.Windows.Forms.ColumnHeader cSize;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.btnFind = new System.Windows.Forms.Button();
+            this.fbdSearchFile = new System.Windows.Forms.FolderBrowserDialog();
+            this.bgwFound = new System.ComponentModel.BackgroundWorker();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.lblSearch = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lvwSearch = new System.Windows.Forms.ListView();
+            this.pbrSearch = new System.Windows.Forms.ProgressBar();
+            this.lblPercent = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.adbPDF = new AxAcroPDFLib.AxAcroPDF();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            cName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            cPath = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            cSize = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            ((System.ComponentModel.ISupportInitialize)(this.adbPDF)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cName
+            // 
+            cName.Text = "Tên file";
+            cName.Width = 237;
+            // 
+            // cPath
+            // 
+            cPath.Text = "Đường dẫn";
+            cPath.Width = 435;
+            // 
+            // cSize
+            // 
+            cSize.Text = "Dung lượng";
+            cSize.Width = 124;
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(588, 58);
+            this.btnFind.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(112, 35);
+            this.btnFind.TabIndex = 1;
+            this.btnFind.Text = "Tìm kiếm";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(166, 60);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(354, 26);
+            this.txtSearch.TabIndex = 2;
+            this.txtSearch.Click += new System.EventHandler(this.txtSearch_Click);
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Location = new System.Drawing.Point(45, 65);
+            this.lblSearch.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(88, 20);
+            this.lblSearch.TabIndex = 3;
+            this.lblSearch.Text = "Đường dẫn";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(45, 151);
+            this.lblName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(60, 20);
+            this.lblName.TabIndex = 4;
+            this.lblName.Text = "Tên file";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(166, 148);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(354, 26);
+            this.txtName.TabIndex = 5;
+            // 
+            // lvwSearch
+            // 
+            this.lvwSearch.Alignment = System.Windows.Forms.ListViewAlignment.Left;
+            this.lvwSearch.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            cName,
+            cPath,
+            cSize});
+            this.lvwSearch.GridLines = true;
+            this.lvwSearch.HideSelection = false;
+            this.lvwSearch.Location = new System.Drawing.Point(-3, 286);
+            this.lvwSearch.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.lvwSearch.Name = "lvwSearch";
+            this.lvwSearch.Size = new System.Drawing.Size(1200, 401);
+            this.lvwSearch.TabIndex = 6;
+            this.lvwSearch.UseCompatibleStateImageBehavior = false;
+            this.lvwSearch.View = System.Windows.Forms.View.Details;
+            this.lvwSearch.SelectedIndexChanged += new System.EventHandler(this.lvwSearch_SelectedIndexChanged);
+            this.lvwSearch.Click += new System.EventHandler(this.lvwSearch_Click);
+            this.lvwSearch.DoubleClick += new System.EventHandler(this.lvwSearch_DoubleClick);
+            // 
+            // pbrSearch
+            // 
+            this.pbrSearch.Location = new System.Drawing.Point(50, 205);
+            this.pbrSearch.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pbrSearch.Name = "pbrSearch";
+            this.pbrSearch.Size = new System.Drawing.Size(501, 35);
+            this.pbrSearch.TabIndex = 7;
+            // 
+            // lblPercent
+            // 
+            this.lblPercent.AutoSize = true;
+            this.lblPercent.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPercent.Location = new System.Drawing.Point(560, 206);
+            this.lblPercent.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPercent.Name = "lblPercent";
+            this.lblPercent.Size = new System.Drawing.Size(54, 29);
+            this.lblPercent.TabIndex = 8;
+            this.lblPercent.Text = "0 %";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(44, 251);
+            this.lblStatus.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(121, 29);
+            this.lblStatus.TabIndex = 9;
+            this.lblStatus.Text = "Trạng thái";
+            // 
+            // adbPDF
+            // 
+            this.adbPDF.Enabled = true;
+            this.adbPDF.Location = new System.Drawing.Point(0, 0);
+            this.adbPDF.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.adbPDF.Name = "adbPDF";
+            this.adbPDF.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("adbPDF.OcxState")));
+            this.adbPDF.Size = new System.Drawing.Size(801, 449);
+            this.adbPDF.TabIndex = 10;
+            this.adbPDF.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(973, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(256, 26);
+            this.textBox1.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(841, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 20);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Tên file";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(841, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 20);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Tên file";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(973, 151);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(256, 26);
+            this.textBox2.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(841, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 20);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Thời gian tạo";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(973, 105);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(256, 26);
+            this.textBox3.TabIndex = 15;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(841, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 20);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Kích thước";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(973, 58);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(256, 26);
+            this.textBox4.TabIndex = 17;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1609, 692);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblPercent);
+            this.Controls.Add(this.pbrSearch);
+            this.Controls.Add(this.lvwSearch);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.adbPDF);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "MainForm";
+            this.Text = "Đọc file PDF";
+            ((System.ComponentModel.ISupportInitialize)(this.adbPDF)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.FolderBrowserDialog fbdSearchFile;
+        private System.ComponentModel.BackgroundWorker bgwFound;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label lblSearch;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.ListView lvwSearch;
+        private System.Windows.Forms.ProgressBar pbrSearch;
+        private System.Windows.Forms.Label lblPercent;
+        private System.Windows.Forms.Label lblStatus;
+        private AxAcroPDFLib.AxAcroPDF adbPDF;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,164 @@
+﻿//Nhóm 8
+//Nguyễn Quang Hùng - 18133016
+//Ngô Trí Huy - 18133015
+//Võ Thị Thanh Ngân - 18133033
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KidleTeam8
+{
+    public partial class MainForm : Form
+    {
+        private BackgroundWorker backgroundWorker;
+        public MainForm()
+        {
+            InitializeComponent();
+            backgroundWorker = new BackgroundWorker();
+            backgroundWorker.WorkerReportsProgress = true;
+            backgroundWorker.WorkerSupportsCancellation = true;
+
+            backgroundWorker.DoWork += BackgroundWorker_DoWork;
+            backgroundWorker.ProgressChanged += BackgroundWorker_ProgressChanged;
+            backgroundWorker.RunWorkerCompleted += BackgroundWorker_RunWorkerCompleted;
+        }
+        #region Xử lý phần tìm file với đuôi .pdf
+        private void BackgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            lblStatus.Text = string.Format("Tìm thấy {0} tập tin", lvwSearch.Items.Count);
+            if (pbrSearch.Value < pbrSearch.Maximum)
+            {
+                lblStatus.Text = "Dừng tìm kiếm";
+            }
+            btnFind.Text = "Tìm kiếm";
+        }
+
+        private void BackgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if(!backgroundWorker.CancellationPending)
+            {
+                lblPercent.Text = e.ProgressPercentage + " %";
+                pbrSearch.PerformStep();
+            }
+        }
+
+        private void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string[] dirs = Directory.GetDirectories(txtSearch.Text);
+            float length = dirs.Length;
+            pbrSearch.Invoke((Action)(() => pbrSearch.Maximum = dirs.Length));
+            ScanDirectory(txtSearch.Text, txtName.Text);
+            for(int i =0; i< dirs.Length;i++)
+            {
+                backgroundWorker.ReportProgress((int)(i / length * 100));
+                ScanDirectory(dirs[i], txtName.Text);
+            }
+            backgroundWorker.ReportProgress(100);
+        }
+
+        public void AddToListView(string file)
+        {
+            FileInfo fileif = new FileInfo(file);
+            lvwSearch.Invoke((Action)(() =>
+            {
+                string key = Path.GetExtension(file);
+                if(key == ".pdf")
+                {
+                    ListViewItem item = new ListViewItem(fileif.Name, key);
+                    item.SubItems.Add(fileif.DirectoryName);
+                    item.SubItems.Add(Math.Ceiling(fileif.Length / 1024f).ToString("0 KB"));
+                    lvwSearch.BeginUpdate();
+                    lvwSearch.Items.Add(item);
+                    lvwSearch.EndUpdate();
+                }               
+            }));
+        }
+
+        public void ScanDirectory(string directory, string searchpatten)
+        {
+            try
+            {
+                foreach(var file in Directory.GetFiles(directory))
+                {
+                    if(backgroundWorker.CancellationPending)
+                    {
+                        return;
+                    }
+                    lblStatus.Invoke((Action)(() =>lblStatus.Text = file));
+                    if(file.Contains(searchpatten))
+                    {
+                        AddToListView(file);
+                    }
+                }
+                foreach(var dir in Directory.GetDirectories(directory))
+                {
+                    ScanDirectory(dir, searchpatten);
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            if(backgroundWorker.IsBusy)
+            {
+                backgroundWorker.CancelAsync();
+            }
+            else
+            {
+                pbrSearch.Value = pbrSearch.Minimum;
+                btnFind.Text = "Dừng tìm";
+                lvwSearch.Items.Clear();
+                backgroundWorker.RunWorkerAsync();
+            }
+        }
+
+        private void txtSearch_Click(object sender, EventArgs e)
+        {
+            if (fbdSearchFile.ShowDialog() == DialogResult.OK)
+            {
+                txtSearch.Text = fbdSearchFile.SelectedPath;
+            }
+        }
+        #endregion
+
+        private void lvwSearch_DoubleClick(object sender, EventArgs e)
+        {
+            adbPDF.Visible = true;
+            adbPDF.BringToFront();
+            OpenFileDialog file = new OpenFileDialog();
+            file.InitialDirectory = lvwSearch.SelectedItems[0].SubItems[1].Text;
+            adbPDF.src = lvwSearch.SelectedItems[0].SubItems[1].Text + "/" + lvwSearch.SelectedItems[0].SubItems[0].Text;
+        }
+
+        private void lvwSearch_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lvwSearch_Click(object sender, EventArgs e)
+        {
+            string path = lvwSearch.SelectedItems[0].SubItems[1].Text + "/" + lvwSearch.SelectedItems[0].SubItems[0].Text;
+            FileInfo ThongTinTruyXuat = new FileInfo(path); // FileInfo thuộc thư viện System.IO
+            if (!ThongTinTruyXuat.Exists)
+            {
+                throw new FileNotFoundException("Không tìm thấy tập tin cần thiết: " + path);
+            }
+            textBox1.Text = ThongTinTruyXuat.Name 
+            + ThongTinTruyXuat.CreationTime.ToLongTimeString()
+            +  ThongTinTruyXuat.LastWriteTime.ToLongDateString()
+            +ThongTinTruyXuat.LastAccessTime.ToLongDateString()
+            + ThongTinTruyXuat.Length.ToString() + " bytes";
+        }
+    }
+}
